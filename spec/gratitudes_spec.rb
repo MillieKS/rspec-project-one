470b39ca1d405fa0aa90857a3eb435196c3becbf
MillieKS <mkseverino95@gmail.com>
2022-08-17 06:22:39 -0700

    Add method test in grattitude

--- spec/gratitudes_spec.rb
+++ spec/gratitudes_spec.rb
@@ -6,8 +6,12 @@ RSpec.describe Gratitudes do
     result = gratitudes
     expect(result).to eq []
   end
+  it "adds a gratitude" do
+    gratitudes = Gratitudes.new
+    expect(gratitudes.add("sunny weather")).to eq ["sunny weather"]
+  end
 end 
 # pushing into the array
-def add(gratitude)
-    @gratitudes.push(gratitude)
-  end
+# def add(gratitude)
+#     @gratitudes.push(gratitude)
+#   end
