a618c6046bba24767f6fe5bea53f9479aa323beb
MillieKS <mkseverino95@gmail.com>
2022-08-17 07:32:29 -0700

    Added format method test.
    Co-authored-by: D. Dramchev <ddramchev@gmail.com>

--- spec/gratitudes_spec.rb
+++ spec/gratitudes_spec.rb
@@ -6,12 +6,34 @@ RSpec.describe Gratitudes do
     result = gratitudes
     expect(result).to eq []
   end
+
+  # it "adds a gratitude" do
+  #   gratitudes = Gratitudes.new
+  #   expect(gratitudes.add("sunny weather")).to eq ["sunny weather"]
+  # end
+
   it "adds a gratitude" do
     gratitudes = Gratitudes.new
-    expect(gratitudes.add("sunny weather")).to eq ["sunny weather"]
+    result = gratitudes.add("sunny weather")
+    expect(result).to eq ["sunny weather"]
+  end
+
+  it "joins gratitude with formatted text" do
+    gratitudes = Gratitudes.new
+    gratitudes.add("cats")
+    gratitudes.add("dogs")
+    gratitudes.add("snakes")
+    result = gratitudes.format 
+    expect(result).to eq "Be grateful for: cats, dogs, snakes"
   end
 end 
+
 # pushing into the array
 # def add(gratitude)
 #     @gratitudes.push(gratitude)
 #   end
+
+# def format
+#   formatted = "Be grateful for: "
+#   formatted += @gratitudes.join(", ")
+# end
