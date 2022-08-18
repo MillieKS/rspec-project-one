4763ae6590dd6ea4b306e45bc05095ff23d3e257
MillieKS <mkseverino95@gmail.com>
2022-08-18 07:01:01 -0700

    Present exercise fail test for contents wrapped.
    LeroyHJ <leroy_jallow_@hotmail.co.uk>

--- spec/present_spec.rb
+++ spec/present_spec.rb
@@ -1,4 +1,11 @@
 require 'present'
 
 RSpec.describe Present do
-  context ""
+  context "A contents has already been wrapped." do
+    it "fails " do
+      gift = Present.new
+      gift.wrap("toy")
+      expect {gift.wrap(@contents)}.to raise_error "A contents has already been wrapped."
+    end
+  end
+end
