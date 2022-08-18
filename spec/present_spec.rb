2a248776fc621306b5712dca2806c5dc4eed8c2a
MillieKS <mkseverino95@gmail.com>
2022-08-18 07:52:36 -0700

    Present exercise no contents wrapped test added.
    Co-authored-by: ddrmv <ddrmv@users.noreply.github.com>
    Co-authored-by: LeroyHJ <LeroyHJ@users.noreply.github.com>

--- spec/present_spec.rb
+++ spec/present_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe Present do
       expect {gift.wrap(@contents)}.to raise_error "A contents has already been wrapped."
     end
   end
+
+  context "No contents has been wrapped." do 
+    it "fails" do
+      gift = Present.new()
+      expect { gift.unwrap }.to raise_error "No contents have been wrapped."
+    end
+  end
 end
